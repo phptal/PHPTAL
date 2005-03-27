4ebcc91c1bd7c1c5d2d19da165053df1c25ae3d1
Laurent Bedubourg <lbedubourg@motion-twin.com>
2005-03-27 15:21:21 +0000

    doc generation script

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -6,15 +6,23 @@ for lang in $LANGS
 do
     echo "* generating $lang"
     make -C $lang
+done
+
+for lang in $LANGS
+do
     if test ! -d $DEST/$lang
     then
         echo "* creating $DEST/$lang"
         mkdir $DEST/$lang
+    fi
+    if test ! -d $DEST/$lang/split
+    then
+        echo "* creating $DEST/$lang"
         mkdir $DEST/$lang/split
     fi
 
     cp style.css $DEST/$lang/
-    cp style.css $DEST/$lang/split
+    cp style.css $DEST/$lang/split/
 
     cp $lang/plaintext/book.txt $DEST/$lang/phptal.txt
     cp $lang/xhtml/* $DEST/$lang/split/
