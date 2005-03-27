4742a070d3878c7779ed9fa401ccefa538edabf1
Laurent Bedubourg <lbedubourg@motion-twin.com>
2005-03-27 15:18:34 +0000

    organizing things

---
+++ doc/gendoc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+LANGS="en"
+DEST=../website/www/manual
+
+for lang in $LANGS
+do
+    echo "* generating $lang"
+    make -C $lang
+    if test ! -d $DEST/$lang
+    then
+        echo "* creating $DEST/$lang"
+        mkdir $DEST/$lang
+        mkdir $DEST/$lang/split
+    fi
+
+    cp style.css $DEST/$lang/
+    cp style.css $DEST/$lang/split
+
+    cp $lang/plaintext/book.txt $DEST/$lang/phptal.txt
+    cp $lang/xhtml/* $DEST/$lang/split/
+    cp $lang/xhtmlonepage/book.html $DEST/$lang/index.html
+done
