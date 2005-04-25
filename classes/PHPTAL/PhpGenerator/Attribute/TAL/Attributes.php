4190b659d778bd1b8b9ab005c541b015480d2a6c
Laurent Bedubourg <lbedubourg@motion-twin.com>
2005-04-25 21:00:25 +0000

    refactorings

--- classes/PHPTAL/PhpGenerator/Attribute/TAL/Attributes.php
+++ classes/PHPTAL/PhpGenerator/Attribute/TAL/Attributes.php
@@ -45,15 +45,16 @@ class PHPTAL_Attribute_TAL_Attributes extends PHPTAL_Attribute
 {
     const ATT_FULL_REPLACE = '$__ATT_';
     const ATT_VALUE_REPLACE = '$__att_';
-
     // this regex is used to determine if an attribute is entirely replaced
     // by a php variable or if only its value is replaced.
     const REGEX_FULL_REPLACE = '/<?php echo \$__ATT_.*? ?>/';
     
     public function start()
     {//{{{
-        $expressions = $this->tag->generator->splitExpression($this->expression);
-        foreach ($expressions as $exp) {
+        // retrieve the list of attributes to replace splitting the
+        // expression using ; delimiter
+        $attrs = $this->tag->generator->splitExpression($this->expression);
+        foreach ($attrs as $exp) {
             list($attribute, $expression) = $this->parseExpression($exp);
             if ($expression) {
                 $this->prepareAttribute($attribute, $expression);
@@ -68,48 +69,44 @@ class PHPTAL_Attribute_TAL_Attributes extends PHPTAL_Attribute
     private function prepareAttribute($attribute, $expression)
     {//{{{
         $code = $this->tag->generator->evaluateExpression($expression);
+
+        // if $code is an array then the attribute value is decided by a
+        // tales chained expression
         if (is_array($code)) {
-            $this->tag->generator->noThrow(true);
-            $this->generateChainedAttribute($attribute, $code);
-            $this->tag->generator->noThrow(false);
-            return;
+            return $this->prepareChainedAttribute($attribute, $code);
         }
        
         // XHTML boolean attribute does not appear when empty of false
         if (PHPTAL_Defs::isBooleanAttribute($attribute)) {
-            $attkey = self::ATT_FULL_REPLACE.$this->getVarName($attribute);
-            $value  = sprintf('" %s=\"%s\""', $attribute, $attribute);
-            $this->tag->generator->doIf($code);
-            $this->tag->generator->doSetVar($attkey, $value);
-            $this->tag->generator->doElse();
-            $this->tag->generator->doSetVar($attkey, '\'\'');
-            $this->tag->generator->doEnd();
+            return $this->prepareBooleanAttribute($attribute, $code);
         }
+        
         // regular attribute which value is the evaluation of $code
-        else {
-            $attkey = self::ATT_VALUE_REPLACE.$this->getVarName($attribute);
-            $value  = $this->tag->generator->escapeCode($code);
-            $this->tag->generator->doSetVar($attkey, $value);
-        }
-        $this->tag->attributes[$attribute] = '<?php echo '.$attkey.' ?>';
-        $this->tag->overwrittenAttributes[$attribute] = $attkey;
+        $attkey = self::ATT_VALUE_REPLACE.$this->getVarName($attribute);
+        $value  = $this->tag->generator->escapeCode($code);
+        $this->tag->generator->doSetVar($attkey, $value);
+        $this->overwriteAttribute($attribute, $attkey);
     }//}}}
 
-    private function generateChainedAttribute($attribute, $chain)
+    private function prepareChainedAttribute($attribute, $chain)
     {//{{{
+        // TODO: chained XHTML boolean attributes
+
         // default attribute value (from source tag)
         $default = false;
         if (array_key_exists($attribute, $this->tag->attributes)) {
             $default = $this->tag->attributes[$attribute];
         }
         
+        // boolean indicating if the chain if/elseif/else started
+        $started = false;
+
         // full attribute replace, the code will decide wether or not the
         // attribute will appear
         $attkey = self::ATT_FULL_REPLACE.$this->getVarName($attribute);
-        // boolean indicating if the if/elseif/else started
-        $started = false;
+        
+        $this->tag->generator->noThrow(true);
         foreach ($chain as $exp){
-           
             // nothing keyword gives an empty attribute value and ends the
             // chain.
             if ($exp == PHPTAL_TALES_NOTHING_KEYWORD){
@@ -144,8 +141,26 @@ class PHPTAL_Attribute_TAL_Attributes extends PHPTAL_Attribute
             $value = "' $attribute=\"'.$value.'\"'";
             $this->tag->generator->doSetVar($attkey, $value);
         }
-       
         $this->tag->generator->doEnd();
+
+        $this->overwriteAttribute($attribute, $attkey);        
+        $this->tag->generator->noThrow(false);
+    }//}}}
+
+    private function prepareBooleanAttribute($attribute, $code)
+    {//{{{
+        $attkey = self::ATT_FULL_REPLACE.$this->getVarName($attribute);
+        $value  = sprintf('" %s=\"%s\""', $attribute, $attribute);
+        $this->tag->generator->doIf($code);
+        $this->tag->generator->doSetVar($attkey, $value);
+        $this->tag->generator->doElse();
+        $this->tag->generator->doSetVar($attkey, '\'\'');
+        $this->tag->generator->doEnd();
+        $this->overwriteAttribute($attribute, $attkey);
+    }//}}}
+
+    private function overwriteAttribute($attribute, $attkey)
+    {//{{{
         $this->tag->attributes[$attribute] = '<?php echo '.$attkey.' ?>';
         $this->tag->overwrittenAttributes[$attribute] = $attkey;
     }//}}}
