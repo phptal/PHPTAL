eeaa92cd5cb9fcaa68db3c6900c2ec5032b82694
Kornel Lesiński <kornel@aardvarkmedia.co.uk>
2009-04-28 12:14:07 +0000

    Split RepeatController

--- classes/PHPTAL/RepeatController.php
+++ classes/PHPTAL/RepeatController.php
@@ -13,6 +13,7 @@
  * @version  SVN: $Id$
  * @link     http://phptal.org/
  */
+  
 /**
  * Stores tal:repeat information during template execution.
  *
@@ -33,11 +34,13 @@
  */
 class PHPTAL_RepeatController implements Iterator
 {
-    private $key;
+    public $key;
     private $current;
     private $valid;
     private $validOnNext;
 
+    private $uses_groups = false;
+    
     protected $iterator;
     public $index;
     public $end;
@@ -77,8 +80,6 @@ class PHPTAL_RepeatController implements Iterator
         } else {
             $this->iterator = new ArrayIterator( array() );
         }
-
-        $this->groups = new PHPTAL_RepeatController_Groups();
     }
 
     /**
@@ -158,8 +159,11 @@ class PHPTAL_RepeatController implements Iterator
             $this->validOnNext = false;
         }
 
-        // Notify the grouping helper of the change
-        $this->groups->reset();
+        if ($this->uses_groups)
+        {
+            // Notify the grouping helper of the change
+            $this->groups->reset();
+        }
     }
 
     /**
@@ -173,8 +177,31 @@ class PHPTAL_RepeatController implements Iterator
         // Prefetch the next element
         if ($this->validOnNext) $this->prefetch();
 
-        // Notify the grouping helper of the change
-        $this->groups->reset();
+        if ($this->uses_groups)
+        {
+            // Notify the grouping helper of the change
+            $this->groups->reset();
+        }
+    }
+
+    /**
+     * Ensures that $this->groups works.
+     *
+     * Groups are rarely-used feature, which is why they're lazily loaded.
+     */
+    private function initializeGroups()
+    {
+        if (!$this->uses_groups)
+        {
+            if (!class_exists('PHPTAL_RepeatControllerGroups',false))
+            {
+                PHPTAL::setIncludePath();
+                require_once "PHPTAL/RepeatControllerGroups.php";
+                PHPTAL::restoreIncludePath();
+            }
+            $this->groups = new PHPTAL_RepeatControllerGroups();
+            $this->uses_groups = true;
+        }
     }
 
     /**
@@ -193,8 +220,6 @@ class PHPTAL_RepeatController implements Iterator
                 return ($this->index % 2) === 0;
             case 'odd':
                 return ($this->index % 2) === 1;
-            case 'key':
-                return $this->key;
             case 'length':
                 return $this->length();
             case 'letter':
@@ -206,11 +231,18 @@ class PHPTAL_RepeatController implements Iterator
             case 'Roman':
                 return strtoupper( $this->int2roman($this->index+1) );
 
+            case 'groups':
+                $this->initializeGroups();
+                return $this->groups;
+
             case 'first':
+                $this->initializeGroups();
                 // Compare the current one with the previous in the dictionary
                 $res = $this->groups->first( $this->current );
                 return is_bool($res) ? $res : $this->groups;
+                
             case 'last':
+                $this->initializeGroups();            
                 // Compare the next one with the dictionary
                 $res = $this->groups->last( $this->iterator->current() );
                 return is_bool($res) ? $res : $this->groups;
@@ -297,184 +329,3 @@ class PHPTAL_RepeatController implements Iterator
     }
 }
 
-
-/**
- * Keeps track of variable contents when using grouping in a path (first/ and last/)
- *
- * @package PHPTAL
- * @subpackage php
- */
-class PHPTAL_RepeatController_Groups
-{
-    protected $dict = array();
-    protected $cache = array();
-    protected $data = null;
-    protected $vars = array();
-    protected $branch;
-
-
-    public function __construct()
-    {
-        $this->dict = array();
-        $this->reset();
-    }
-
-    /**
-     * Resets the result caches. Use it to signal an iteration in the loop
-     *
-     */
-    public function reset()
-    {
-        $this->cache = array();
-    }
-
-    /**
-     * Checks if the data passed is the first one in a group
-     *
-     * @param mixed $data   The data to evaluate
-     * @return Mixed    True if the first item in the group, false if not and
-     *                  this same object if the path is not finished
-     */
-    public function first( $data )
-    {
-        if ( !is_array($data) && !is_object($data) && !is_null($data) ) {
-
-            if ( !isset($this->cache['F']) ) {
-
-                $hash = md5($data);
-
-                if ( !isset($this->dict['F']) || $this->dict['F'] !== $hash ) {
-                    $this->dict['F'] = $hash;
-                    $res = true;
-                } else {
-                    $res = false;
-                }
-
-                $this->cache['F'] = $res;
-            }
-
-            return $this->cache['F'];
-        }
-
-        $this->data = $data;
-        $this->branch = 'F';
-        $this->vars = array();
-        return $this;
-    }
-
-    /**
-     * Checks if the data passed is the last one in a group
-     *
-     * @param mixed $data   The data to evaluate
-     * @return Mixed    True if the last item in the group, false if not and
-     *                  this same object if the path is not finished
-     */
-    public function last( $data )
-    {
-        if ( !is_array($data) && !is_object($data) && !is_null($data) ) {
-
-            if ( !isset($this->cache['L']) ) {
-
-                $hash = md5($data);
-
-                if (empty($this->dict['L'])) {
-                    $this->dict['L'] = $hash;
-                    $res = false;
-                } elseif ( $this->dict['L'] !== $hash ) {
-                    $this->dict['L'] = $hash;
-                    $res = true;
-                } else {
-                    $res = false;
-                }
-
-                $this->cache['L'] = $res;
-            }
-
-            return $this->cache['L'];
-        }
-
-        $this->data = $data;
-        $this->branch = 'L';
-        $this->vars = array();
-        return $this;
-    }
-
-    /**
-     * Handles variable accesses for the tal path resolver
-     *
-     * @param string $var   The variable name to check
-     * @return Mixed    An object/array if the path is not over or a boolean
-     *
-     * @todo    replace the phptal_path() with custom code
-     */
-    public function __get( $var )
-    {
-        // When the iterator item is empty we just let the tal
-        // expression consume by continuously returning this
-        // same object which should evaluate to true for 'last'
-        if ( is_null($this->data) ) {
-            return $this;
-        }
-
-        // Find the requested variable
-        $value = phptal_path( $this->data, $var, true );
-
-        // Check if it's an object or an array
-        if ( is_array($value) || is_object($value) ) {
-            // Move the context to the requested variable and return
-            $this->data = $value;
-            $this->addVarName( $var );
-            return $this;
-        }
-
-        // get a hash of the variable contents
-        $hash = md5( $value );
-
-        // compute a path for the variable to use as dictionary key
-        $path = $this->branch . $this->getVarPath() . $var;
-
-        // If we don't know about this var store in the dictionary
-        if ( !isset($this->cache[$path]) ) {
-
-            if ( !isset($this->dict[$path]) ) {
-                $this->dict[$path] = $hash;
-                $res = $this->branch === 'F';
-            } else {
-                // Check if the value has changed
-                if ( $this->dict[$path] !== $hash ) {
-                    $this->dict[$path] = $hash;
-                    $res = true;
-                } else {
-                    $res = false;
-                }
-            }
-
-            $this->cache[$path] = $res;
-        }
-
-        return $this->cache[$path];
-
-    }
-
-    /**
-     * Adds a variable name to the current path of variables
-     *
-     * @param string $varname  The variable name to store as a path part
-     * @access protected
-     */
-    protected function addVarName( $varname )
-    {
-        $this->vars[] = $varname;
-    }
-
-    /**
-     * Returns the current variable path separated by a slash
-     *
-     * @return String  The current variable path
-     * @access protected
-     */
-    protected function getVarPath()
-    {
-        return implode('/', $this->vars) . '/';
-    }
-}
