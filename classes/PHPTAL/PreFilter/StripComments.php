bec41865b0977a5d8c2f5c9c19a10896711e7904
Kornel Lesiński <kornel@aardvarkmedia.co.uk>
2009-10-24 22:34:48 +0000

    Used isCDATAElementInHTML() in StripComments prefilter

--- classes/PHPTAL/PreFilter/StripComments.php
+++ classes/PHPTAL/PreFilter/StripComments.php
@@ -4,13 +4,13 @@ class PHPTAL_PreFilter_StripComments extends PHPTAL_PreFilter
 {
     function filterDOM(PHPTAL_Dom_Element $element)
     {
+        $defs = PHPTAL_Dom_Defs::getInstance();
+        
         foreach($element->childNodes as $node) {
             if ($node instanceof PHPTAL_Dom_Comment) {
-                $ns = $element->getNamespaceURI();
-                $ln = $element->getLocalName();
-                if (($ns === '' || $ns === 'http://www.w3.org/1999/xhtml') && ($ln === 'script' || $ln === 'style')) {                    
+                if ($defs->isCDATAElementInHTML($element->getNamespaceURI(),$element->getLocalName())) {                    
                     $textNode = new PHPTAL_Dom_CDATASection($node->getValueEscaped(), $node->getEncoding());                    
-                    $node->parentNode->replaceChild($textNode,$node);                    
+                    $node->parentNode->replaceChild($textNode,$node);
                 } else {
                     $node->parentNode->removeChild($node);
                 }
