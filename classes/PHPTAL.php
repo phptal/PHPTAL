9c22c46fd499a6a3bbebb1d13b19c76f3bb894f9
Kornel Lesiński <kornel@aardvarkmedia.co.uk>
2009-10-27 12:00:18 +0000

    Version bump

--- classes/PHPTAL.php
+++ classes/PHPTAL.php
@@ -13,7 +13,7 @@
  * @link     http://phptal.org/
  */
 
-define('PHPTAL_VERSION', '1_2_1b4');
+define('PHPTAL_VERSION', '1_2_2a1');
 
 
 /* If you want to use autoload, comment out all lines starting with require_once 'PHPTAL
